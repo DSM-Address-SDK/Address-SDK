910c150c1ffa4f00c1273482eaa47a4fe32658f5
inung1004 <dksdbswl10430@naver.com>
2023-10-06 20:28:59 +0900

    fix: min-height으로 변경

--- packages/service/src/components/TipBox.tsx
+++ packages/service/src/components/TipBox.tsx
@@ -2,7 +2,7 @@ import { TIPS } from "constants/tipbox";
 
 const TipBox = () => {
   return (
-    <div className="px-9 py-12 bg-white h-[600px] w-full flex flex-col justify-start">
+    <div className="px-9 py-12 bg-white min-h-[600px] w-full flex flex-col justify-start">
       <div className="flex flex-col gap-2">
         <p className="text-black text-[24px] font-bold leading-[31px]">Tip</p>
         <p className="text-black text-[14px] font-regular leading-[22px]">
