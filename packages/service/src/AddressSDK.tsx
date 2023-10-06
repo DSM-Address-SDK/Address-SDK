2e10441d40ee497de8fd55c3d657fbd63b8482ae
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-10-06 17:04:25 +0900

    feat: Input 컴포넌트 퍼블리싱

--- packages/service/src/AddressSDK.tsx
+++ packages/service/src/AddressSDK.tsx
@@ -1,5 +1,18 @@
+import Input from "components/Input";
+import { useInput } from "hooks/useInput";
+
 function AddressSDK() {
-  return <></>;
+  const {
+    form: search,
+    onChange: onChangeSearch,
+    setForm: setSearch,
+  } = useInput("");
+
+  return (
+    <>
+      <Input value={search} onChange={onChangeSearch} setState={setSearch} />
+    </>
+  );
 }
 
 export default AddressSDK;
