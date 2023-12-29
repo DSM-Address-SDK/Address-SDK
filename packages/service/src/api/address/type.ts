c7774ca5bd483b1a35fad2df8dddec3c8de5b688
kimjh11130 <kimjh11130@naver.com>
2023-12-29 15:56:40 +0900

    fix: type 수정

--- packages/service/src/api/address/type.ts
+++ packages/service/src/api/address/type.ts
@@ -10,13 +10,16 @@ export type LanguageType = {
 export type AddressType = {
   // 0 : 대표 주소 = 도로명, 1 : 대표 주소 = 지번, 2 : 사서함
   type: 0 | 1 | 2;
-  postal_code: string;
-  represent_address_name: string;
+  postalCode: string;
+  representAddressNameKor: string;
+  representAddressNameEng: string;
   jibuns: LanguageType[];
-  road_names: LanguageType[];
-  post: string | null;
 };
 
 export interface SearchAddressResponse {
   items: AddressType[];
+}
+
+export interface CountAddressResponse {
+  total_count: number;
 }
